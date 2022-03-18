44eb8aa86a4306cad181bd4bb59dd1a18d935ddd
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2022-03-18 12:25:37 +0100

    change changelog to .md

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_consentform';
-$plugin->version = 2022011200;
+$plugin->version = 2022031800;
 $plugin->release = 'v3.11.0';
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_STABLE;
