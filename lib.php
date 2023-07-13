0bb471f8e7221206434348f9349b6f3d08d2c9ed
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2023-07-13 08:05:16 +0200

    fix the height and enlarge the width

--- lib.php
+++ lib.php
@@ -98,9 +98,11 @@ function consentform_add_instance(stdClass $consentform, mod_consentform_mod_for
         $url = $CFG->wwwroot."/mod/consentform/confirmation.php?id=".$consentform->id;
         $iframeparms["src"] = $url;
         $iframeparms["scrolling"] = "no";
-        $iframeparms["onload"] = "this.style.height=this.contentWindow.document.documentElement.scrollHeight + 'px';";
+        $js = "this.style.height=this.contentWindow.document.documentElement.scrollHeight + 'px';";
+        $js .= "this.contentWindow.document.getElementById('page').style.marginTop='0px';";
+        $iframeparms["onload"] = $js;
         $iframeparms["frameborder"] = "0";
-        $iframeparms["style"] = "min-width:450px;";
+        $iframeparms["class"] = "w-75";
         $iframeparms["name"] = "consentformiframe$consentform->id";
         $html = html_writer::tag("iframe", null, $iframeparms);
         $consentformintro = $html;
