90c71840a0ea1b84689371507f2f61db45deb745
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2024-01-06 15:36:56 +0100

    change height of page 4

--- lib.php
+++ lib.php
@@ -98,7 +98,7 @@ function consentform_add_instance(stdClass $consentform, mod_consentform_mod_for
         $url = $CFG->wwwroot."/mod/consentform/confirmation.php?id=".$consentform->id;
         $iframeparms["src"] = $url;
         $iframeparms["scrolling"] = "auto";
-        $js = "this.style.height=(this.contentWindow.document.documentElement.scrollHeight*0.87).toString() + 'px';";
+        $js = "this.style.height=(this.contentWindow.document.documentElement.scrollHeight*0.87-10).toString() + 'px';";
         $js .= "this.contentWindow.document.getElementById('page').style.marginTop='0px';";
         $iframeparms["onload"] = $js;
         $iframeparms["frameborder"] = "0";
