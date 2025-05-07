805247ab3ea674c99a0c8cac3239c8c503d27dd5
Clemens <clemens.marx@gmail.com>
2025-05-07 16:16:18 +0200

    #8272 Prepared plugin for Moodle version 5.0

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_consentform';
-$plugin->version = 2024101200;
-$plugin->release = 'v4.5.0';
-$plugin->requires = 2024100700;
+$plugin->version = 2025050700;
+$plugin->release = 'v5.0.0';
+$plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
